a9c94184d939eadbb3607899daf6b6c409e0ab87
jordidbmf <jordi@zamplex.com.br>
2023-09-08 16:57:52 -0300

    first commit

---
+++ src/automoveis/Autodromo.java
@@ -0,0 +1,15 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        //jeep.ligar();
+        jeep.setChassi("890");
+
+        Moto z400 = new Moto();
+        //z400.ligar();
+        z400.setChassi("123");
+
+        Veiculo coringa = jeep;
+
+        coringa.ligar();
+    }
+}
